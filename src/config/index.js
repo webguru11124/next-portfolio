43e0c206fb8cd497f98661156d484790e72df6d0
lucas <lucas.eriksson.pro@gmail.com>
2023-07-15 23:06:07 +0900

    initial commit

---
+++ src/config/index.js
@@ -0,0 +1,52 @@
+/* eslint-disable max-len */
+import featuredProjects from './featured';
+import projects from './projects';
+
+module.exports = {
+  email: 'jrgarciadev@gmail.com',
+  featuredProjects,
+  projects,
+  skills: ['JavaScript', 'TypeScript', 'React Native', 'React', 'Next.js', 'GraphQL'],
+  socialMedia: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/webguru11124',
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/lucas-webguru/',
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.linkedin.com/in/lucas-webguru/',
+    },
+    {
+      name: 'Twitter',
+      url: 'https://www.linkedin.com/in/lucas-webguru/',
+    },
+  ],
+
+  navLinks: [
+    {
+      name: 'About',
+      url: '/#about',
+    },
+    {
+      name: 'Blog',
+      url: 'https://www.linkedin.com/in/lucas-webguru/',
+    },
+    {
+      name: 'Work',
+      url: '/#projects',
+    },
+    {
+      name: 'Contact',
+      url: '/#contact',
+    },
+  ],
+  colors: {
+    green: '#64ffda',
+    navy: '#0a192f',
+    darkNavy: '#020c1b',
+  },
+};
